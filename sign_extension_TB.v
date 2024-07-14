0af1dc90938d166417fe75ea3a3205fa69c7f2e9
Mohamed Dawod <138527948+MohamedDawod29@users.noreply.github.com>
2024-07-15 00:28:40 +0300

    Update sign_extension_TB.v

--- sign_extension_TB.v
+++ sign_extension_TB.v
@@ -3,13 +3,16 @@ module sign_extension_TB;
 	parameter width = 32;	
 		
 	//define TB signals
-	reg [11:0] Instr_TB;                    
+	reg [24:0] Instr_TB;                               
+	reg [1:0] ImmSrc_TB;
 	wire [width-1:0] ImmExt_TB;
+	
 	
 	//instantiation of Module
 	sign_extension #(.width(width)) DUT
 	(
 	.Instr(Instr_TB),
+	.ImmSrc(ImmSrc_TB),
 	.ImmExt(ImmExt_TB)
 	);
 	
@@ -17,7 +20,8 @@ module sign_extension_TB;
 	//initial values and test cases
 	initial 
 	begin
-		Instr_TB = 12'h000;                         // h for hexa decimal type (000 ---> 0000 0000 0000)
+		Instr_TB = 25'b0000000000000000000010100; 
+		ImmSrc_TB = 2'b00;
 		#10
 		if (ImmExt_TB == 32'h00000000)
 			$display ("Test Case 1 is passed ImmExt_TB = %h",ImmExt_TB);
@@ -25,20 +29,28 @@ module sign_extension_TB;
 			$display ("Test Case 1 is not passed ImmExt_TB = %h",ImmExt_TB);
 		
 		#10
-		Instr_TB = 12'h001;
+		ImmSrc_TB = 2'b01;
 		#10
-		if (ImmExt_TB == 32'h00000001)
+		if (ImmExt_TB == 32'h00000014)
 			$display ("Test Case 2 is passed ImmExt_TB = %h",ImmExt_TB);
 		else
 			$display ("Test Case 2 is not passed ImmExt_TB = %h",ImmExt_TB);
 		
 		#10
-		Instr_TB = 12'hF10;
+		ImmSrc_TB = 2'b10;
 		#10
-		if (ImmExt_TB == 32'hFFFFFF10)
+		if (ImmExt_TB == 32'h00000014)
 			$display ("Test Case 3 is passed ImmExt_TB = %h",ImmExt_TB);
 		else
 			$display ("Test Case 3 is not passed ImmExt_TB = %h",ImmExt_TB);
+			
+		#10
+		ImmSrc_TB = 2'b11;
+		#10
+		if (ImmExt_TB == 32'h00000000)
+			$display ("Test Case 4 is passed ImmExt_TB = %h",ImmExt_TB);
+		else
+			$display ("Test Case 4 is not passed ImmExt_TB = %h",ImmExt_TB);
 			
 		#10
 		$stop;
@@ -46,4 +58,3 @@ module sign_extension_TB;
 	
 endmodule
 
-	
