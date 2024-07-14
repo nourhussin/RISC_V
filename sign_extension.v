8697ba5d553ccdb4c1181ab2e3351a99795e0bf4
Mohamed Dawod <138527948+MohamedDawod29@users.noreply.github.com>
2024-07-15 00:28:02 +0300

    Update sign_extension.v

--- sign_extension.v
+++ sign_extension.v
@@ -1,11 +1,18 @@
 module sign_extension #(parameter width = 32)
 (
-	input wire [11:0] Instr,                       //bits [31:20] of isntruction
+	input wire [24:0] Instr,  	 //maps Instr [31:7] from the instruction memory
+	input wire [1:0] ImmSrc,        //selector to select the extension operation for a specific type
 	output reg [width-1:0] ImmExt
 );
 
-	always @(Instr)
+	always @(*)
 	begin
-			ImmExt = {{20{Instr[11]}},Instr};             //extend the Instr to be 32 bits instead of 12 bits
+		case (ImmSrc)
+		2'b00: ImmExt = {{20{Instr[24]}},Instr[24:13]};             //extension for I type
+		2'b01: ImmExt = {{20{Instr[24]}},Instr[24:18],Instr[4:0]};             //extension for S type
+		2'b10: ImmExt = {{20{Instr[24]}},Instr[0],Instr[23:18],Instr[4:1],1'b0};             //extension for B type
+		2'b11: ImmExt = {{12{Instr[24]}},Instr[12:5],Instr[13],Instr[23:14],1'b0};             //extension for J type
+		endcase
 	end
+	
 endmodule 
