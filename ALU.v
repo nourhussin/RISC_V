0e4dbc5419646daff1eb59825a3305682d20ccef
mmariam23 <146178416+mmariam23@users.noreply.github.com>
2024-07-19 20:10:40 +0300

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -1,32 +1,25 @@
-module ALU (clk,reset,ScrA,ScrB,AluControl,zero,ALUResult);
+module ALU (ScrA,ScrB,AluControl,zero,ALUResult);
 
-input [15:0] ScrA,ScrB;
-input clk,reset;
+input [width-1:0] ScrA,ScrB;
 input [2:0] AluControl;
-output reg zero;
-output reg  [31:0] ALUResult;                        // how many bit in the result?
+output zero;
+output [width-1:0] ALUResult;                        // how many bit in the result?
 
 parameter width =32;
 
-always @(posedge clk,negedge reset) begin
-       if (reset) begin 
-		        ALUResult<= 32'b0;
-		 end
-       else begin 
-              case(AluControl)
-			         3'b000: ALUResult <= ScrA & ScrB ;                
-			         3'b001: ALUResult <= ScrA | ScrB ;
-			         3'b010: ALUResult <= ScrA + ScrB ;		
-			         3'b100: ALUResult <= ScrA & ~ScrB ;	 
-			         3'b101: ALUResult <= ScrA | ~ScrB ;	
-			         3'b110: ALUResult <= ScrA - ScrB ;	
-			         3'b111: if (ScrA <ScrB) begin
-									ALUResult<=32'b0000_0000_0000_0001;
-								  end else
-								   ALUResult<= 32'b0;
-			         default: ALUResult <=32'b0;
-					endcase
-		 end			
+always @(*) begin
+case(AluControl)
+  3'b010: ALUResult <= ScrA & ScrB ;                
+  3'b011: ALUResult <= ScrA | ScrB ;
+  3'b000: ALUResult <= ScrA + ScrB ;			 	 
+  3'b001: ALUResult <= ScrA - ScrB ;	
+  3'b101: if (ScrA <ScrB) begin
+		ALUResult<=32'b0000_0000_0000_0001;
+		end else
+		    ALUResult<= 32'b0;
+   default: ALUResult <=32'b0;
+endcase
+					
 end
 endmodule		 
 	  
