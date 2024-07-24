8b00f6978c9f151c3e99175464c2daa012970c2c
Mohamed Dawod <138527948+MohamedDawod29@users.noreply.github.com>
2024-07-25 02:11:14 +0300

    Update ALU_Decoder.v

--- ALU_Decoder.v
+++ ALU_Decoder.v
@@ -6,29 +6,42 @@ module ALU_Decoder
 	input wire [1:0] ALUOp,
 	output reg [2:0] ALUControl
 );
-always@*
+
+always@(*)
 begin
 
-case(ALUOp)
-	2'b00:ALUControl=3'b000;///lw,sw
-	2'b01:ALUControl=3'b001;///beq
+casex (ALUOp)
+	2'b00:ALUControl=3'b000;///lw,sw,jalr
+	
+	2'b01:
+	begin
+	casex (funct3)
+		3'b000: ALUControl = 3'b001;///beq
+		3'b001: ALUControl = 3'b001;///bne
+		default : ALUControl = 3'bxxx;
+	endcase
+	end
+	
 	2'b10:
-		begin
-		case(funct3)
+	begin
+		casex (funct3)
 			3'b000:
 			begin
-				if({op5,funct7_5}==2'b11)
-				ALUControl=3'b001;//sub
+				if({op5,funct7_5} == 2'b11)
+					ALUControl=3'b001;//sub
 				else 
-				ALUControl=3'b000;//add
+					ALUControl=3'b000;//add,addi
 			end
-			3'b010:ALUControl=3'b101;//slt
-			3'b110:ALUControl=3'b011;//or
-			3'b111:ALUControl=3'b010;//and
-			default:ALUControl=3'b000;
+			
+			//3'b010:ALUControl=3'b101;//slt not required
+			3'b110:ALUControl=3'b011;//or,ori
+			3'b111:ALUControl=3'b010;//and,andi
+			default:ALUControl=3'bxxx;
 		endcase
-		end
-	default:ALUControl=3'b000;
+	end
+	default:ALUControl=3'bxxx;
 endcase
+
 end
+
 endmodule
