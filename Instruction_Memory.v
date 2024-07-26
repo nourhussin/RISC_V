322f36aaa81eb6bb93840a2eb2b0532f219e7d04
Nour Hussin <n0ur0eldiin@gmail.com>
2024-07-26 23:33:59 +0300

    Add cache memory

--- Instruction_Memory.v
+++ Instruction_Memory.v
@@ -14,12 +14,13 @@ module Instruction_Memory#(parameter depth = 256, width = 32)(
         memory[0] = 32'h00500113;	//addi x2 x0 5	 x2 = 5                     0           00500113
         memory[1] = 32'h00c00193;	//addi x3 x0 12	 x3 = 12                    4           00C00193
         memory[2] = 32'hff718393;	//addi x7 x3 -9	 x7 = (12 - 9) = 3          8           FF718393
-        memory[3] = 32'h0023e233;	//or x4 x7 x2	 x4 = (3 OR 5) = 7          C           0023E233
+        memory[3] = 32'h0023e233;	//or x4 x7 x2	 x4 = (3 OR 5) = 7            C           0023E233
         memory[4] = 32'h0041f2b3;	//and x5 x3 x4	 x5 = (12 AND 7) = 4        10          0041F2B3
         memory[5] = 32'h004282b3;	//add x5 x5 x4	 x5 = 4 + 7 = 11            14          004282B3
         memory[6] = 32'h02728863;	//beq x5 x7 48	 shouldn't be taken         18          02728863
-      //memory[6] = 32'h02729863;  // bne x5 x7 48  should be taken             48          02729863
-      // bne is tested and it works fine :)
+        //memory[6] = 32'h02729863;  // bne x5 x7 48  should be taken         48          02729863
+        //memory[6] = 32'h000000e7 ; // jalr x0,x1,0 
+      // bne and jalr are tested and it works fine :)
         memory[7] = 32'h0041a233;	//slt x4 x3 x4	 x4 = (12 < 7) = 0          1C          0041A233
         memory[8] = 32'h00020463;	//beq x4 x0 8	 should be taken            20          00020463
         memory[9] = 32'h00000293;	//addi x5 x0 0	 shouldn't execute          24          00000293
--- cache_memory.v
+++ cache_memory.v
@@ -3,14 +3,54 @@ module cache_memory #(parameter cache_width=128,
                                 memory_width=32, 
                                 memory_depth=1024)(
 
-    input wire clk, reset_n, fill,
-    input wire[$clog2(memory_depth)-1:0] A,
-    input wire[memory_width-1:0] PWD,   // processor write data 1 word
-    input wire[cache_width-1 :0] MWD,   // memory write data    4 words
+    input wire clk, refill, update,
+    input wire[1:0] offset,
+    input wire[4:0] index,
+    input wire[cache_width-1:0] line_data,   
+    input wire[memory_width-1 :0] write_data,
 
-    output wire hit,
-    output wire[memory_width-1:0] PRD,  // processor read data  1 word
-    output wire[memory_width-1:0] MRD   // memory read data     4 words
-);
+    output reg[memory_width-1:0] read_data
+    );
 
+    wire [cache_width-1 : 0] read_cache_line;
+    reg [cache_width-1 : 0] write_cache_line;
+    reg [cache_width-1 : 0] cache_mem [cache_depth-1 : 0];
+
+
+    assign read_cache_line = cache_mem[index];
+    always @(*)
+    begin
+        case (offset)
+        2'b00:
+        begin
+            read_data = read_cache_line[31:0];
+            write_cache_line = {read_cache_line[127:32], write_data};
+        end
+        2'b01:
+        begin
+            read_data = read_cache_line[63:32];
+            write_cache_line = {read_cache_line[127:64], write_data, read_cache_line[31:0]};
+
+        end
+        2'b10:
+        begin
+            read_data = read_cache_line[95:64];
+            write_cache_line = {read_cache_line[127:96], write_data, read_cache_line[63:0]};
+        end
+        2'b11:
+        begin
+            read_data = read_cache_line[127:96];
+            write_cache_line = {write_data, read_cache_line[95:0]};
+        end
+        endcase
+    end
+
+    always @(posedge clk)
+    begin
+        if(refill)
+            cache_mem[index] <= line_data;
+        else if(update)
+            cache_mem[index] <= write_cache_line;
+
+    end
 endmodule
